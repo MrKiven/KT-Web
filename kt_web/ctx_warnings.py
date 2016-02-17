0f26f7a16c5595fe093b4e42a5574dd970a10366
kiven <kiven.mr@gmail.com>
2016-02-17 17:19:30 +0800

    add a `warning` common

---
+++ kt_web/ctx_warnings.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+import contextlib
+import warnings
+import functools
+
+
+class CatchWarnMixin(object):
+    """Catch Warnings by a contextmanager or a decorator
+    Usage:
+
+        @CatchWarnMixin.warning()
+        def a_func():
+            warnings.warn('some warning')
+
+        or
+
+        with CatchWarnMixin.catchWarnings():
+            warnings.warn('some warning')
+    """
+
+    @staticmethod
+    @contextlib.contextmanager
+    def catchWarnings(action='default'):
+        warnings.simplefilter(action)
+        yield
+        warnings.resetwarnings()
+
+    @staticmethod
+    def warning(action='default'):
+        def so(func):
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                warnings.simplefilter(action)
+                return func(*args, **kwargs)
+            return wrapper
+        return so
