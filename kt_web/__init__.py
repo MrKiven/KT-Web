f908c09b5272359413dc75aadc3a0c5b77477481
kiven <kiven.mr@gmail.com>
2016-02-22 11:47:45 +0800

    refactor api

--- kt_web/__init__.py
+++ kt_web/__init__.py
@@ -1,4 +1,16 @@
 # -*- coding:utf-8 -*-
 
+from app import app
+
+import logging
+import logging.config
+import yaml
+
+logging.config.dictConfig(yaml.load(open('logging.conf')))
+logfile = logging.getLogger('file')
+logconsole = logging.getLogger('console')
+
+app.logger.info('Logging is set up')
+
 version_info = (0, 0, 1)
 __version__ = ".".join([str(v) for v in version_info])
--- kt_web/api/__init__.py
+++ kt_web/api/__init__.py
@@ -1,13 +1,1 @@
 # -*- coding:utf-8 -*-
-
-import logging
-import logging.config
-import yaml
-
-from app import app
-
-logging.config.dictConfig(yaml.load(open('logging.conf')))
-logfile = logging.getLogger('file')
-logconsole = logging.getLogger('console')
-
-app.logger.info('Logging is set up')
