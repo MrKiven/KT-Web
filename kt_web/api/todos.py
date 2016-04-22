b90f6ed4fcf71fd162a264b8252a87552ced140f
kiven <kiven.mr@gmail.com>
2016-04-22 16:55:39 +0800

    add reqparse

--- kt_web/api/todos.py
+++ kt_web/api/todos.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 
 from flask import Blueprint, request
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 from exc import NotFoundResourceException
 
@@ -13,7 +13,18 @@ bp = Blueprint('todos', __name__)
 api = Api(bp)
 
 
-class Todo(Resource):
+class ParserMixin(object):
+    """参数解析Mixin
+    """
+
+    @staticmethod
+    def get_key(key, required=False):
+        parser = reqparse.RequestParser()
+        parser.add_argument(key, type=str, help="", required=required)
+        return parser.parse_args().get(key)
+
+
+class Todo(Resource, ParserMixin):
     def get(self, todo_id):
         todo = todos.get(todo_id, None)
         if todo is None:
@@ -21,7 +32,9 @@ class Todo(Resource):
         return todo
 
     def put(self, todo_id):
-        todos[todo_id] = request.form['data']
+        data = self.get_key('data', required=True)
+        # todos[todo_id] = request.form['data']
+        todos[todo_id] = data
         return {todo_id: todos[todo_id]}
 
 
