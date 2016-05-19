2e58b7ba2f6031ed82ee58b7bfbfa02765f1293d
kiven <kiven.mr@gmail.com>
2016-05-19 18:52:07 +0800

    Use `SKT` Framework to run app

--- kt_web/__init__.py
+++ kt_web/__init__.py
@@ -1,16 +1,17 @@
 # -*- coding:utf-8 -*-
 
+import logging
+
 from app import app
 
-import logging
-import logging.config
-import yaml
+# import logging.config
+# import yaml
 
-logging.config.dictConfig(yaml.load(open('logging.conf')))
-logfile = logging.getLogger('file')
-logconsole = logging.getLogger('console')
-
-app.logger.info('Logging is set up')
+# logging.config.dictConfig(yaml.load(open('logging.conf')))
+# logfile = logging.getLogger('file')
+# logconsole = logging.getLogger('console')
+logger = logging.getLogger(__name__)
+logger.info('Logging is set up')
 
 version_info = (0, 0, 1)
 __version__ = ".".join([str(v) for v in version_info])
